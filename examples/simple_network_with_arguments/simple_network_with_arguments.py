220e4b74f10dcfd9d530e63b047ebc07aa60703e
Tomáš Mikula <mail@tomasmikula.cz>
2020-10-19 15:57:02 +0200

    Add argument example

---
+++ examples/simple_network_with_arguments/simple_network_with_arguments.py
@@ -0,0 +1,40 @@
+import time
+import pathlib
+from multiprocessing import current_process
+
+from distsim import Network
+
+
+def node_code(arg):
+    # get current node object
+    node = current_process()
+
+    # get logger
+    logger = node.get_logger()
+
+    logger.info(f"Starting node.")
+
+    logger.info(f"Argument is: {arg}")
+
+    logger.info(f"Shutting down.")
+
+
+# defing network architecture
+NETWORK_ARCHITECTURE = {
+    'node1': {
+        'out': {'node2', },
+        'function': node_code,
+        'args': ('argument for node 1', )},
+    'node2': {
+        'out': {'node1', },
+        'function': node_code,
+        'args': ('argument for node 2', )},
+}
+
+# run network
+if __name__ == "__main__":
+    network = Network(NETWORK_ARCHITECTURE,
+                      log_dir=pathlib.Path(__file__).parent.absolute())
+
+    network.start()
+    network.join()
