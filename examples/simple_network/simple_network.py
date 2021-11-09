e2dac454a2be691527f35c471b76cec30090d9fd
Tomáš Mikula <mail@tomasmikula.cz>
2021-11-09 08:54:15 +0100

    Update example

--- examples/simple_network/simple_network.py
+++ examples/simple_network/simple_network.py
@@ -6,21 +6,17 @@ from distsim import Network, Node, Link
 
 
 def node_code():
-    # get current node object
     node = current_process()
 
-    # get logger
     logger = node.init_logger()
 
     logger.info(f"Starting node.")
 
-    # send msg to each neighbors
     for node_name in node.neighbors:
         msg = f"Msg from node {node.name}"
         node.send_to(node_name, msg)
         logger.info(f"Sent {msg} to {node_name}.")
 
-    # recieve msg from each input pipelines
     for node_name in node.neighbors:
         msg, _ = node.recv_from(node_name)
         logger.info(f"Recv {msg} from {node_name}.")
@@ -29,7 +25,7 @@ def node_code():
 
     logger.info(f"Shutting down.")
 
-# run network
+
 if __name__ == "__main__":
     nodes = (
         Node("node1", node_code),
